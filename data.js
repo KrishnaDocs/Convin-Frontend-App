67a2f12554e23a92829a8a55c75323320e0e7fa7
SaiKrishna <117157361+KrishnaDocs@users.noreply.github.com>
2023-03-27 00:10:13 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,19 @@
+export const data = [
+    {
+      name: "JavaScript",
+      img: "https://pluspng.com/img-png/javascript-vector-png-javascript-vector-logo-600.png",
+    },
+    {
+      name: "neo",
+      img: "https://imgur.com/RF2a3PB.jpg",
+    },
+    {
+      name: "morpheus",
+      img: "https://imgur.com/B0SNpZI.jpg",
+    },
+    {
+      name: "trinity",
+      img: "https://imgur.com/KnXHM0K.jpg",
+    },
+  ];
+
